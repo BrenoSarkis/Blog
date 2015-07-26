e9a1e86ae5a0dd391e351e26d3828a1f6dca128d
brenosarkis <sarkisbreno@gmail.com>
2015-07-26 16:35:20 -0300

    Adicionando os templates

---
+++ Blog.Web/Controllers/AdministradorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        public ActionResult NovoPost()
+        {
+            return View();
+        }
+    }
+}
