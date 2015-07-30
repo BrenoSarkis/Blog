458784175fbf983cd59f579e1e3c4a9a22c226c3
brenosarkis <sarkisbreno@gmail.com>
2015-07-30 19:44:51 -0300

    Tags com angular js

---
+++ Blog.Web/Scripts/app/PostController.js
@@ -0,0 +1,13 @@
+﻿angular.module('PostApp', [])
+    .controller('PostController', function ($scope, $http) {
+        $scope.tags = [];
+
+        $scope.adicionarTag = function () {
+            $scope.tags.push($scope.tag);
+        }
+
+        $scope.removerTag = function (indice) {
+            $scope.tags.splice(indice, 1);
+        }
+    })
+
